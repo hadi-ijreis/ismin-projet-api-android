b332c8b934343a217878e203b6067425e713bdfc
hadi-ijreis <hadiijreis@gmail.com>
2021-11-06 16:37:37 +0100

    tried to add process.env.PORT in main

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,6 +5,6 @@ import { CrousModule } from './crous.module';
 async function bootstrap() {
   const app = await NestFactory.create(CrousModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
